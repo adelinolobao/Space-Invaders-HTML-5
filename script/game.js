e171066bdc6439ef6aa44c473820b71da18f588b
Adelino Lobão <sousalobao@gmail.com>
2012-01-14 18:36:47 +0000

    added comments and organized code

--- script/game.js
+++ script/game.js
@@ -1,50 +1,63 @@
 /**
  * File game.js
- * implement by Adelino Lobão
- * 09/10/2012
+ * implemented by Adelino Lobão
+ * 14/01/2012
  */
 
-//definition of window
-var width = 600;
-var height = 800;
-
-var gameLoop;
-
-var c = document.getElementById("content");
-var ctx = c.getContext("2d");
-
-c.width = width;
-c.height = height;
-
-var drawScene = function() {
-	ctx.fillStyle = '#000';
-	ctx.clearRect(0, 0, width, height);
-	ctx.beginPath();
-	ctx.rect(0, 0, width, height);
-	ctx.closePath();
-	ctx.fill();
+/**
+ * Draw the window game
+ */
+var drawWindow = function() {
+ 	context.fillStyle = '#000';
+	context.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	context.beginPath();
+	context.rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	context.closePath();
+	context.fill();
 }
+
+/**
+ * Run the game
+ */
+var runGame = function() {
+ 	//draw the window game
+ 	drawWindow();
+ 	//draw the player
+	player.draw(context);
+	//set timeout function
+	gameLoop = setTimeout(runGame, intervalTime);
+}
+
+//window width
+const WINDOW_WIDTH = 600;
+//window height
+const WINDOW_HEIGHT = 800;
+//frame rate
+const FRAME_RATE = 50;
+
+//game loop
+var gameLoop;
+//interval time
+var intervalTime = 1000 / FRAME_RATE;
+
+//get frame window
+var frameWindow = document.getElementById("content");
+//frame window width
+frameWindow.width = WINDOW_WIDTH;
+//frame window height
+frameWindow.height = WINDOW_HEIGHT;
+//get context
+var context = frameWindow.getContext("2d");
 
 //keyhandler object
 var keyHandler = new KeyHandler();
 //player object
-var player = new Player(width, height);
+var player = new Player(WINDOW_WIDTH, WINDOW_HEIGHT);
 
 //handle events when the a key is pressed
 document.onkeypress = function(e){
 	keyHandler.keyPress(e, player);	
 }
 
-var RunGame = function() {
-	//draw the scene
-	drawScene();
-
-	//draw the player
-	player.draw(ctx);
-
-	const FRAME_RATE = 50;
-	var intervalTime = 1000 / FRAME_RATE;	
-	gameLoop = setTimeout(RunGame, intervalTime);
-}
-
-RunGame();
+//run the game
+runGame();
